a92cdc48b299e90eac112b30d6bb0419f2af53e6
Israel Flores <iflore04@gmail.com>
2017-04-03 23:38:55 -0700

    Added initial template

--- app/app.py
+++ app/app.py
@@ -3,7 +3,6 @@ from werkzeug.serving import run_simple
 from werkzeug.debug import DebuggedApplication
 from time import sleep
 from controllers.db_adapter import db
-from models.tables import Group, Managed, Upload, GlobalGroup, Retrieval
 import json
 import requests
 from sqlalchemy.orm import load_only
@@ -13,9 +12,7 @@ from functools import wraps
 import hashlib
 
 
-REGION_NAME = "us-east-1"
-VERSION = 0.9
-DOWN_REVISION = 0.5
+VERSION = 0.1
 app = Flask(__name__)
 
 
@@ -24,13 +21,14 @@ def not_found(e):
     return render_template('404.html'), 404
 
 
-@app.route("/Glacier/", methods=["GET"])
+@app.route("/", methods=["GET"])
 def landing():
 
-    return render_template("main/landing.html")
+    return render_template("main/index.html")
 
 
 if __name__ == "__main__":
 
     app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-    app.run()
+    app.config.update(TEMPLATES_AUTO_RELOAD=True)
+    app.run(host="0.0.0.0", port=8080, debug=True, threaded=True)
