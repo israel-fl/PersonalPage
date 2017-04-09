600ea6d6ea294d4bffbed4dfbf241d4c439dd83a
Israel Flores <iflore04@gmail.com>
2017-04-09 08:00:45 -0700

    Finished most of the site

--- app/app.py
+++ app/app.py
@@ -1,19 +1,17 @@
 from flask import Flask, render_template, request, flash, redirect, url_for, jsonify
+from flask_socketio import SocketIO, emit
 from werkzeug.serving import run_simple
 from werkzeug.debug import DebuggedApplication
 from time import sleep
-from controllers.db_adapter import db
-import json
-import requests
+# from controllers.db_adapter import db
 from sqlalchemy.orm import load_only
-import datetime
 from sqlalchemy import desc
 from functools import wraps
-import hashlib
 
 
 VERSION = 0.1
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 
 @app.errorhandler(404)
@@ -21,14 +19,32 @@ def not_found(e):
     return render_template('404.html'), 404
 
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def landing():
-
+    if (request.method == "POST"):
+        name = request.form.get("name")
+        email = request.form.get("email")
+        phone = request.form.get("phone")
+        message = request.form.get("message")
+        pass
     return render_template("main/index.html")
 
+
+@app.route('/data', methods=["GET"])
+def render():
+    return render_template("main/data.html")
+
+
+@socketio.on('message', namespace='/test')
+def handle_message(message):
+    emit('MESSAGE', {'data': message['data'], 'user': 'SAMMY', 'position': 'left'}, broadcast=True)
+
+@socketio.on('broadcast', namespace='/test')
+def test_message(message):
+    emit('MESSAGE', {'data': message['data'], 'user': "visitor", 'position': 'right'}, broadcast=True)
 
 if __name__ == "__main__":
 
     app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
     app.config.update(TEMPLATES_AUTO_RELOAD=True)
-    app.run(host="0.0.0.0", port=8080, debug=True, threaded=True)
+    socketio.run(app, host="0.0.0.0", port=8080, debug=True)
--- app/controllers/db_adapter.py
+++ app/controllers/db_adapter.py
@@ -1,33 +1,33 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-import json
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+# import json
 
-with open("app/controllers/config.json") as config_file:
-# with open("C:\inetpub\Glacier\controllers\config.json") as config_file:
-    keys = json.load(config_file)
-    driver = keys.get("db_driver", "postgresql")
-    user = keys.get("db_user", "postgres")
-    password = keys.get("db_pass", "admin")
-    host = keys.get("db_host", "localhost")
-    port = keys.get("db_port", "5432")
-    name = keys.get("db_name", "development")
+# with open("app/controllers/config.json") as config_file:
+# # with open("C:\inetpub\Glacier\controllers\config.json") as config_file:
+#     keys = json.load(config_file)
+#     driver = keys.get("db_driver", "postgresql")
+#     user = keys.get("db_user", "postgres")
+#     password = keys.get("db_pass", "admin")
+#     host = keys.get("db_host", "localhost")
+#     port = keys.get("db_port", "5432")
+#     name = keys.get("db_name", "development")
 
-engine = create_engine(
-    '{driver}://{user}:{password}@{db_host}:{port}/{db_name}'.format(
-        driver=driver,
-        user=user,
-        password=password,
-        db_host=host,
-        port=port,
-        db_name=name
-    )
-)
+# engine = create_engine(
+#     '{driver}://{user}:{password}@{db_host}:{port}/{db_name}'.format(
+#         driver=driver,
+#         user=user,
+#         password=password,
+#         db_host=host,
+#         port=port,
+#         db_name=name
+#     )
+# )
 
-Session = sessionmaker(bind=engine)
+# Session = sessionmaker(bind=engine)
 
-db = Session()
+# db = Session()
 
-Base = declarative_base()
+# Base = declarative_base()
 
-Base.metadata.create_all(engine)
+# Base.metadata.create_all(engine)
