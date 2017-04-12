e857ad9ea9e1ae1281e3b10a1535ea08a9b0a6c9
Israel Flores <iflore04@gmail.com>
2017-04-11 22:18:26 -0700

    Added functionality to send emails

--- app/app.py
+++ app/app.py
@@ -7,17 +7,23 @@ from time import sleep
 from sqlalchemy.orm import load_only
 from sqlalchemy import desc
 from functools import wraps
+import json
 
 
 VERSION = 0.1
 app = Flask(__name__)
 socketio = SocketIO(app)
-app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'iflore04@gmail.com'
-app.config['MAIL_PASSWORD'] = 'ce651edaf59be5ef2badb77d2833f3feac9fbab8abd4cc8b5e1af7921f58a9db'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+
+with open("config.json") as config_file:
+    keys = json.load(config_file)
+    app.config['MAIL_PASSWORD'] = keys.get("MAIL_PASSWORD")
+    app.config['MAIL_USERNAME'] = keys.get("MAIL_USERNAME")
+    app.config['MAIL_PORT'] = keys.get("MAIL_PORT")
+    app.config['MAIL_SERVER'] = keys.get("MAIL_SERVER")
+    app.config['MAIL_USE_TLS'] = keys.get("MAIL_USE_TLS")
+    app.config['MAIL_USE_SSL'] = keys.get("MAIL_USE_SSL")
 
 @app.errorhandler(404)
 def not_found(e):
@@ -31,7 +37,19 @@ def landing():
         email = request.form.get("email")
         phone = request.form.get("phone")
         message = request.form.get("message")
-        pass
+        mail = Mail(app)
+        email = Message(
+                subject="A new message from IsraelFL!",
+                sender="iflore04@gmail.com",
+                recipients=email)
+        email.html = render_template("emails/contact_email.html",
+                                     name=name,
+                                     email=email,
+                                     phone=phone,
+                                     message=message)
+
+        mail.send(email)
+
     return render_template("main/index.html")
 
 
@@ -54,12 +72,12 @@ def render():
 def handle_message(message):
     emit('MESSAGE', {'data': message['data'], 'user': 'SAMMY', 'position': 'left'}, broadcast=True)
 
+
 @socketio.on('broadcast', namespace='/test')
 def test_message(message):
     emit('MESSAGE', {'data': message['data'], 'user': "visitor", 'position': 'right'}, broadcast=True)
 
-if __name__ == "__main__":
 
-    app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+if __name__ == "__main__":
     app.config.update(TEMPLATES_AUTO_RELOAD=True)
     socketio.run(app, host="0.0.0.0", port=8080, debug=True)
