1504ed3643b9d07160642212b4e6c32b9ad57d37
Israel Flores <iflore04@gmail.com>
2017-05-07 00:55:20 -0700

    Fixed an issue where data was being loaded before the fold

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,21 +1,14 @@
-$(document).ready(function(){
-    var socket = io.connect('https://' + document.domain + ':' + location.port + '/test', {secure: true});
-
+$(document).ready(function() {
+    var socket = io.connect('https://' + document.domain + ':' + location.port + '/test', { secure: true });
     socket.on('MESSAGE', function(msg) {
         var clear = document.createElement("div");
         var body = document.createElement("div");
         var message = document.createElement("p");
         var bubbleClass = 'from-me';
-
         $('#btn-input').val('');
         $('#btn-input2').val('');
-
         clear.setAttribute("class", "clear-chat");
-
-
-        if (msg.position === 'left') {
-            bubbleClass = 'from-them';
-        }
+        if (msg.position === 'left') { bubbleClass = 'from-them'; }
         body.setAttribute("class", bubbleClass);
         message.innerHTML = msg.data;
         body.append(message);
@@ -23,15 +16,13 @@ $(document).ready(function(){
         $(body).appendTo('#chatbox, #chatbox2');
         $('#holder').scrollTop($('#holder')[0].scrollHeight);
         $('#holder2').scrollTop($('#holder2')[0].scrollHeight);
-
     });
     $('#btn-chat').click(function(event) {
-        socket.emit('message', {data: $('#btn-input').val()});
+        socket.emit('message', { data: $('#btn-input').val() });
         return false;
     });
     $('#btn-chat2').click(function(event) {
-        socket.emit('broadcast', {data: $('#btn-input2').val()});
+        socket.emit('broadcast', { data: $('#btn-input2').val() });
         return false;
     });
 });
-
